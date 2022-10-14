b731f4cba222287015434ec6937c6b9f81a0ad4d
Eamon Powell <eamonpowell@outlook.com>
2022-10-14 09:14:44 +0000

    AutoBrightness: Add support for one shot auto-brightness - Only update auto brightness one time when screen is turned on. - Can be useful on devices where sensor is not accurate.
    Author: Eamon Powell <eamonpowell@outlook.com>
    Date:   Sun Aug 29 11:37:44 2021 +1000
    
        fixup! AutoBrightness: Add support for one shot auto-brightness:
    
        * Remove unnecessary newlines
        * Fix incorrect indentation
        * Fix typo in comments
    
        Change-Id: I7d6071f4344605b3dcc3f1c33ac4390f1020e03f
    
    Change-Id: I5187fdb7765550d4eaa999317ab95f487d44c8d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5846,6 +5846,13 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * Whether auto brightness is applied one shot when screen is turned on.
+         * Default 0
+         * @hide
+         */
+        public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
+
+        /**
          * Whether to enable the ripple animation on fingerprint unlock
          * @hide
          */
